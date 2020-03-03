25da3949dfe30c438f7f47974d9ba2005fc7a440
ali <alinaeem2006@gmail.com>
2020-03-03 14:38:24 +1100

    * Built basic foundations of the UI * Added all form fields

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <form className="formContainer">
+        <label>Postcode</label>
+        <input type="text" name="postcode" placeholder="2000" />
+
+        <label>Suburb</label>
+        <input type="text" name="suburb" placeholder="Sydney" />
+
+        <label>State</label>
+        <select id="state" name="state">
+          <option value="NSW">New South Wales</option>
+          <option value="VIC">Victoria</option>
+          <option value="WA">Western Australia</option>
+          <option value="QLD">Queensland</option>
+          <option value="SA">South Australia</option>
+          <option value="TAS">Tasmania</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }

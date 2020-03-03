49a37ab84ff4b923759445549bec33b610ac9576
ali <alinaeem2006@gmail.com>
2020-03-03 15:31:57 +1100

    * Added hooks to control flow of data * Added result box div

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+function validateAddress(location) {
+  console.log(location);
+}
 
 function App() {
+  const [form, setForm] = useState({ state: "NSW" });
+  const [validations, setValidation] = useState({
+    suburb: false,
+    postcode: false,
+    state: false
+  });
+  const [message, setMessage] = useState("");
+
+  const handleInputChange = event => {
+    let obj = {};
+    obj[event.target.name] = event.target.value;
+    setForm({ ...form, ...obj });
+
+    let valid = {};
+    valid[event.target.name] = true
+    setValidation({...validations, ...valid});
+    console.log(form);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    for (const [key,value] of Object.entries(validations)) {
+      if (value === false) {
+        setMessage("Please fill in all fields.")
+        return false;
+      }
+    }
+    validateAddress(form);
+  };
+
   return (
     <div className="mainContainer">
-      <form className="formContainer">
-        <label>Postcode</label>
-        <input type="text" name="postcode" placeholder="2000" />
-
+      <form onSubmit={handleSubmit} className="formContainer">
         <label>Suburb</label>
-        <input type="text" name="suburb" placeholder="Sydney" />
+        <input
+          type="text"
+          name="suburb"
+          placeholder="Sydney"
+          onChange={handleInputChange}
+          onBlur={handleInputChange}
+        />
+
+        <label>Postcode</label>
+        <input
+          type="text"
+          name="postcode"
+          placeholder="2000"
+          onChange={handleInputChange}
+          onBlur={handleInputChange}
+        />
 
         <label>State</label>
-        <select id="state" name="state">
+        <select
+          id="state"
+          name="state"
+          onChange={handleInputChange}
+          onBlur={handleInputChange}
+        >
           <option value="NSW">New South Wales</option>
           <option value="VIC">Victoria</option>
           <option value="WA">Western Australia</option>
@@ -21,8 +70,16 @@ function App() {
           <option value="SA">South Australia</option>
           <option value="TAS">Tasmania</option>
         </select>
-        <button type="submit">Submit</button>
+
+        <button onSubmit={handleSubmit} type="submit">
+          Submit
+        </button>
+
+
       </form>
+      <div className="messageContainer">
+          <span className="resultText">{message}</span>
+        </div>
     </div>
   );
 }
